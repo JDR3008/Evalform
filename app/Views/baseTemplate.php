fb4dce39393ce2253133191fae76178f28641967
James de Raat <jamederaat@gmail.com>
2024-05-07 06:30:19 +0000

    Created Base Template

---
+++ app/Views/baseTemplate.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EvalForm</title>
+    <!-- Import Bootstrap Libraries -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <!-- Import Stylesheet -->
+    <link rel="stylesheet" href="<?= base_url('css/style.css'); ?>">
+    
+</head>
+
+<body class="page-container">
+
+    <main>
+        <!-- Unique page content -->
+        <?= $this->renderSection('content') ?> 
+    </main>
+
+    <!-- Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
